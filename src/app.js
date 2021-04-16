c286e331f605bceddda531fd3927d40cf350e425
Hasan <hasan.doctype@gmail.com>
2021-04-16 16:43:14 +0400

    React Start

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+import './styles/main.scss';
+
+
+ReactDOM.render(<p>Reacy Starter</p>, document.getElementById('root'));
